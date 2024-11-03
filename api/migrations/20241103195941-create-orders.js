e8593197012e1bc92ba1484ccc87a9c4f730b762
manulongh <emanuellibarboza@gmail.com>
2024-11-03 17:02:01 -0300

    migration orders table

---
+++ api/migrations/20241103195941-create-orders.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      tableNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      customer: {
+        type: DataTypes.STRING,
+      },
+      status: {
+        type: DataTypes.INTEGER,
+      },
+      waiterId: {
+        type: DataTypes.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      openedAt: {
+        type: DataTypes.DATE,
+      },
+      closedAt: {
+        type: DataTypes.DATE,
+      },
+      totalAmount: {
+        type: DataTypes.DECIMAL,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
