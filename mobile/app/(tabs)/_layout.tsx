fa59e9bc6417f2950f6d8e6181eed9689952cc24
Lucas Chitolina <lucas.chitolina01@gmail.com>
2024-11-19 20:26:20 -0300

    Revert "Adding request client"

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -32,15 +32,6 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="fetch"
-        options={{
-          title: 'Fetch',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
     </Tabs>
   );
 }
