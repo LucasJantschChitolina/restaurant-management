ca4eb4d3de7ed81355ada53f6b19e6150e20f326
Chitolina <lucas.chitolina01@gmail.com>
2024-11-24 09:32:21 -0300

    :art: add associations for menu items and implement checks for pending production orders and opened orders

--- api/src/modules/menu-item/repositories/menuItemRepository.ts
+++ api/src/modules/menu-item/repositories/menuItemRepository.ts
@@ -1,7 +1,17 @@
-import MenuItemModel, { MenuItemCategory } from '../../../../models/menu-item';
 import sequelize from '../../../db';
 
+import MenuItemModel, { MenuItemCategory } from '../../../../models/menu-item';
+import ProductionOrderModel from '../../../../models/production-order';
+import OrderItemModel from '../../../../models/order-item';
+import OrderModel from '../../../../models/orders';
+
 const MenuItem = MenuItemModel(sequelize);
+const ProductionOrder = ProductionOrderModel(sequelize);
+const OrderItem = OrderItemModel(sequelize);
+const Order = OrderModel(sequelize);
+
+OrderItem.associate({ MenuItem, Order, ProductionOrder });
+ProductionOrder.associate({ OrderItem });
 
 export const createMenuItem = async (data: { description: string; price: number, category: MenuItemCategory }) => {
   return await MenuItem.create(data);
@@ -30,4 +40,38 @@ export const deleteMenuItem = async (id: string) => {
 
 export const listMenuItems = async () => {
   return await MenuItem.findAll();
+};
+
+export const isMenuItemInPendingProductionOrder = async (menuItemId: string) => {
+  const count = await ProductionOrder.count({
+    include: [{
+      model: OrderItem,
+      as: 'orderItems',
+      where: {
+        menuItemId,
+      },
+    }],
+    where: {
+      status: 'PENDING',
+    },
+  });
+
+  return count > 0;
+};
+
+export const isMenuItemInOpenedOrder = async (menuItemId: string) => {
+  const count = await OrderItem.count({
+    where: {
+      menuItemId,
+    },
+    include: [{
+      model: Order,
+      as: 'order',
+      where: {
+        status: 'OPENED',
+      },
+    }],
+  });
+
+  return count > 0;
 };
